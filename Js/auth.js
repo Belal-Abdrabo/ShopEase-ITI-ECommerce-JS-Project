d5ff660de812f7360ec466c2942313af1964a47d
Ahmed Ibrahim <ahmedibrahim000074@gmail.com>
2025-04-29 20:32:17 +0300

    added validation to auth.js

--- Js/auth.js
+++ Js/auth.js
@@ -1,12 +1,59 @@
-let x = 0;
-const login = function name(email, password) {
-    x = x + 1;
-    console.log("Login attempt number: " + x);
-    if (email === "admin" && password === "admin") {
-        console.log("Login successful!");
-        return true;
-    } else {
-        console.log("Login failed!");
-        return false;
-    }
+window.addEventListener('load', function(){
+    const form = document.querySelector("#login-form");
+    const email = document.querySelector("#email");
+    const password = document.querySelector("#password");
+    const emailErrorMessage = document.querySelector("#email-login-error");
+    const passwordErrorMessage = document.querySelector("#password-login-error");
+
+    form.addEventListener('submit', function(event){
+        let flag = true;
+        console.log("submit event triggered");
+        
+        let emailValue = email.value.trim();
+        let passwordValue = password.value.trim();
+        if(emailValue === '')
+        {
+            console.log("Email is empty");
+            event.preventDefault(); // Prevent form submission
+            emailErrorMessage.textContent = "Please fill email field";
+            emailErrorMessage.style.display = "block"; // Show the error message
+            flag = false;
+        }
+        else if(!isValidEmail(emailValue))
+        {
+            event.preventDefault(); // Prevent form submission
+            emailErrorMessage.textContent = "Please enter a valid email address";
+            emailErrorMessage.style.display = "block"; // Show the error message
+            flag = false;
+        }
+        if(passwordValue === '')
+        {
+            console
+            event.preventDefault(); // Prevent form submission
+            passwordErrorMessage.textContent = "Please fill password field";
+            passwordErrorMessage.style.display = "block"; // Show the error message
+            flag = false;
+        }
+        if(flag)
+        {
+            emailErrorMessage.style.display = "none"; // Hide the error message
+            passwordErrorMessage.style.display = "none"; // Hide the error message
+            event.preventDefault();
+            console.log("log in");
+            
+        }
+    });
+});
+
+const isValidEmail = (email_value) =>{
+    console.log('email val: '+email_value);
+    
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/; // Basic email pattern
+    console.log('email val: '+emailPattern.test(email_value));
+    
+    return emailPattern.test(email_value);
+}
+
+const login = (_email, _password) => {
+
 }
